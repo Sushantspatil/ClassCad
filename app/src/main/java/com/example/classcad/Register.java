3b84ed3c315b22edee2659d19c3a83e4bde52dec
Thordark <78201550+someguy2104@users.noreply.github.com>
2021-04-25 03:01:09 +0530

    register page

--- app/src/main/java/com/example/classcad/Register.java
+++ app/src/main/java/com/example/classcad/Register.java
@@ -1,14 +1,81 @@
 package com.example.classcad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class Register extends AppCompatActivity {
+    EditText mName,mEmail,mPass,mEnroll;
+    Button mRegister;
+    TextView mIfreg;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mName = findViewById(R.id.name);
+        mEmail = findViewById(R.id.email);
+        mPass = findViewById(R.id.pass);
+        mEnroll = findViewById(R.id.enroll);
+        mRegister = findViewById(R.id.register);
+        mIfreg = findViewById(R.id.ifreg);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        mRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    mPass.setError("Password is required");
+                }
+                if(pass.length()<6){
+                    mPass.setError("Password must be greater than 6 characters");
+                }
+
+                fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "User createc", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(Register.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+
+        });
+
     }
 }
